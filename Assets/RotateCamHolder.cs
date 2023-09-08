4c6546d08355ed5a4d6c72f14db0e53bc3fe673c
Okan Ataç <okanatac.cl@gmail.com>
2023-09-08 15:43:10 +0300

    camera rot, tower mesh edit, collision fixes

---
+++ Assets/RotateCamHolder.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class RotateCamHolder : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+    }
+}
